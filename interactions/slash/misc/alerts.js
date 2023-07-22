4c28a749bbedb60cb87a91c8be55afa4c2b36d97
VitalityMigo <catwalk7th@gmail.com>
2023-07-22 12:49:56 +0200

    Update alerts.js

--- interactions/slash/misc/alerts.js
+++ interactions/slash/misc/alerts.js
@@ -114,7 +114,7 @@ module.exports = {
 			let serverId = interaction.member.guild.id
 			let member = interaction.member;
 
-			try {
+			//try {
 
 				const communityRolePerms = await accessSql.findOne({ where: { serverId: serverId } })
 				let communityMemberRoleId = communityRolePerms.dataValues.memberRoleId
@@ -1083,6 +1083,8 @@ module.exports = {
 								let floorSide = interaction.options.getString("trend")
 								let authorName = interaction.user.username;
 
+								const removealertEmbed = new EmbedBuilder().setColor("#060A8F")
+
 								//console.log(collection)
 								let userAvatar = "https://cdn.discordapp.com/avatars/" + authorId + "/" + interaction.user.avatar + ".png"
 
@@ -1256,82 +1258,82 @@ module.exports = {
 
 				}
 
-			} catch (error) {
+			// } catch (error) {
 
 
-				console.log("// Error - sent in report ❌")
+			// 	console.log("// Error - sent in report ❌")
 
-				//On envoi une notif
-				let botId = interaction.applicationId
-				const botAdmins = await adminsql.findOne({ where: { botId: botId } })
-				const mainServerId = botAdmins.dataValues.mainServerId
-				const logChannelId = botAdmins.dataValues.logChannelId
-				const guild = interaction.client.guilds.cache.get(mainServerId);
-				const channel = guild.channels.cache.get(logChannelId);
-
-
-				const adminAccessInfos = await accessSql.findOne({ where: { serverId: serverId } })
-				let adminRoleId = adminAccessInfos.dataValues.adminRoleId
-				let serverName = adminAccessInfos.dataValues.serverName
-				const userRoleList = interaction.member._roles
-				let userHighestRole = "Member"
-				if (userRoleList.includes(adminRoleId)) { userHighestRole = "Team" }
-				let reportCommand = "/alerts"
-
-				const timeStamp = Date.now();
-				const date = new Date(timeStamp);
-				const dateLisible = date.toLocaleString();
-				const date1 = moment(dateLisible, 'M/D/YYYY, h:mm:ss A');
-				const formattedDate = date1.format('Do [of] MMMM YYYY');
+			// 	//On envoi une notif
+			// 	let botId = interaction.applicationId
+			// 	const botAdmins = await adminsql.findOne({ where: { botId: botId } })
+			// 	const mainServerId = botAdmins.dataValues.mainServerId
+			// 	const logChannelId = botAdmins.dataValues.logChannelId
+			// 	const guild = interaction.client.guilds.cache.get(mainServerId);
+			// 	const channel = guild.channels.cache.get(logChannelId);
 
 
+			// 	const adminAccessInfos = await accessSql.findOne({ where: { serverId: serverId } })
+			// 	let adminRoleId = adminAccessInfos.dataValues.adminRoleId
+			// 	let serverName = adminAccessInfos.dataValues.serverName
+			// 	const userRoleList = interaction.member._roles
+			// 	let userHighestRole = "Member"
+			// 	if (userRoleList.includes(adminRoleId)) { userHighestRole = "Team" }
+			// 	let reportCommand = "/alerts"
 
-				//On enregistre le call
-				await reportsql.create({
-					botId: botId,
-					authorId: "Bot",
-					serverName: serverName,
-					authorRole: userHighestRole,
-					serverId: serverId,
-					date: formattedDate,
-					reportType: "Bug",
-					reportCommand: reportCommand,
-					reportDescription: "```" + error.stack + "```",
-					reportPriority: "5",
-					reportState: "Not treated",
-				})
+			// 	const timeStamp = Date.now();
+			// 	const date = new Date(timeStamp);
+			// 	const dateLisible = date.toLocaleString();
+			// 	const date1 = moment(dateLisible, 'M/D/YYYY, h:mm:ss A');
+			// 	const formattedDate = date1.format('Do [of] MMMM YYYY');
 
 
 
-				const updateEmbed = new EmbedBuilder().setColor("#060A8F")
-					.setTitle("New Report")
-					.setDescription(">>> A new report has just been sent.")
-					.setThumbnail('https://media.discordapp.net/attachments/949300412874362983/1040242440696758282/Logo_Rolls_V2_5.3_auto_x2.jpg')
-					.setAuthor({ name: "Rolls Chasers Analytics", iconURL: "https://media.discordapp.net/attachments/949300412874362983/1040242440696758282/Logo_Rolls_V2_5.3_auto_x2.jpg" })
-					.setTimestamp()
-					.addFields(
-						{ name: " ", value: " ", inline: false },
-						{ name: "Content:", value: "A new `bug` has been submitted for the `" + reportCommand + "` command by `the bot report division` in `" + serverName + "`. You can use the administrator dashboard to consult it.", inline: false },
-
-					)
-					.setFooter({ text: 'Powered by Rolls Chasers', iconURL: 'https://cdn.discordapp.com/attachments/1108757872315219968/1121978623436521514/rc_logo.png' })
-
-
-				await channel.send({ embeds: [updateEmbed] });
-
-
-				const errorAnswerUser = new EmbedBuilder().setColor("#060A8F")
-					.setTitle("An error occured")
-					.setDescription("An error has occurred while executing this command. These errors can occur for a variety of reasons, such as :\n∙ Unexpected traffic\n∙ API maintenance\n∙ Occasional bug\n\nPlease note that a report has already been sent to our team, who will fix the problem as soon as possible. You can still use `/report` to give more details about the error and help our team.")
-					.setThumbnail('https://media.discordapp.net/attachments/949300412874362983/1040242440696758282/Logo_Rolls_V2_5.3_auto_x2.jpg')
-					.setTimestamp()
-					.setFooter({ text: 'Powered by Rolls Chasers Analytics', iconURL: 'https://media.discordapp.net/attachments/949300412874362983/1040242440696758282/Logo_Rolls_V2_5.3_auto_x2.jpg' })
+			// 	//On enregistre le call
+			// 	await reportsql.create({
+			// 		botId: botId,
+			// 		authorId: "Bot",
+			// 		serverName: serverName,
+			// 		authorRole: userHighestRole,
+			// 		serverId: serverId,
+			// 		date: formattedDate,
+			// 		reportType: "Bug",
+			// 		reportCommand: reportCommand,
+			// 		reportDescription: "```" + error.stack + "```",
+			// 		reportPriority: "5",
+			// 		reportState: "Not treated",
+			// 	})
 
 
-				await interaction.editReply({ embeds: [errorAnswerUser], ephemeral: true });
+
+			// 	const updateEmbed = new EmbedBuilder().setColor("#060A8F")
+			// 		.setTitle("New Report")
+			// 		.setDescription(">>> A new report has just been sent.")
+			// 		.setThumbnail('https://media.discordapp.net/attachments/949300412874362983/1040242440696758282/Logo_Rolls_V2_5.3_auto_x2.jpg')
+			// 		.setAuthor({ name: "Rolls Chasers Analytics", iconURL: "https://media.discordapp.net/attachments/949300412874362983/1040242440696758282/Logo_Rolls_V2_5.3_auto_x2.jpg" })
+			// 		.setTimestamp()
+			// 		.addFields(
+			// 			{ name: " ", value: " ", inline: false },
+			// 			{ name: "Content:", value: "A new `bug` has been submitted for the `" + reportCommand + "` command by `the bot report division` in `" + serverName + "`. You can use the administrator dashboard to consult it.", inline: false },
+
+			// 		)
+			// 		.setFooter({ text: 'Powered by Rolls Chasers', iconURL: 'https://cdn.discordapp.com/attachments/1108757872315219968/1121978623436521514/rc_logo.png' })
 
 
-			}
+			// 	await channel.send({ embeds: [updateEmbed] });
+
+
+			// 	const errorAnswerUser = new EmbedBuilder().setColor("#060A8F")
+			// 		.setTitle("An error occured")
+			// 		.setDescription("An error has occurred while executing this command. These errors can occur for a variety of reasons, such as :\n∙ Unexpected traffic\n∙ API maintenance\n∙ Occasional bug\n\nPlease note that a report has already been sent to our team, who will fix the problem as soon as possible. You can still use `/report` to give more details about the error and help our team.")
+			// 		.setThumbnail('https://media.discordapp.net/attachments/949300412874362983/1040242440696758282/Logo_Rolls_V2_5.3_auto_x2.jpg')
+			// 		.setTimestamp()
+			// 		.setFooter({ text: 'Powered by Rolls Chasers Analytics', iconURL: 'https://media.discordapp.net/attachments/949300412874362983/1040242440696758282/Logo_Rolls_V2_5.3_auto_x2.jpg' })
+
+
+			// 	await interaction.editReply({ embeds: [errorAnswerUser], ephemeral: true });
+
+
+			// }
 
 		} else if (interaction.guildId == null) {
 

0c87a99a8ea82d7fb1bd38f4a4fa978fa07c7e5f
VitalityMigo <catwalk7th@gmail.com>
2023-07-26 12:11:52 +0200

    test

--- interactions/slash/misc/access.js
+++ interactions/slash/misc/access.js
@@ -36,7 +36,7 @@ module.exports = {
                 //Récupère info varibale sur le bot et le serveur
                 const communityRolePerms = await accessSql.findOne({ where: { serverId: serverId } })
                 let communityMemberRoleId = communityRolePerms.dataValues.memberRoleId
-                let communityAdminRoleId = communityRolePerms.dataValues.adminRoleId
+                let communityAdminRoleId = communityRolePerms.dataValues.admiRoleId
                 let botPowerStatut = communityRolePerms.dataValues.actualPower
                 let communityStatut = communityRolePerms.dataValues.statut
 

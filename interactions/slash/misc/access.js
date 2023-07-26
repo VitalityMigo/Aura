5334ce324e2019f6e3bafcdd05d13530a922d86b
VitalityMigo <catwalk7th@gmail.com>
2023-07-26 12:16:27 +0200

    Update access.js

--- interactions/slash/misc/access.js
+++ interactions/slash/misc/access.js
@@ -289,7 +289,7 @@ module.exports = {
                 })
 
 
-                console.log(error)
+                console.log("//////////\n\nDetails de l'erreur :\n\n" + error + "\n\n//////////")
 
                 const reduceText = require("../../../functions/reducetext")
 
@@ -303,7 +303,7 @@ module.exports = {
                         { name: " ", value: " ", inline: false },
                         { name: "Content:", value: "A new `bug` has been submitted for the `" + reportCommand + "` command by `the bot report division` in `" + serverName + "`. You can use the administrator dashboard to consult it.", inline: false },
                         { name: " ", value: " ", inline: false },
-                        { name: "Error:", value: reduceText(error.stack, 1024), inline: false },
+                        { name: "Error:", value: "```" + reduceText(error.stack, 1024) + "```", inline: false },
 
                     )
                     .setFooter({ text: 'Powered by Rolls Chasers', iconURL: 'https://cdn.discordapp.com/attachments/1108757872315219968/1121978623436521514/rc_logo.png' })

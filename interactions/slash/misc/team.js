4982d48c915fc74dc038de9022a697e0b905a4b8
VitalityMigo <catwalk7th@gmail.com>
2023-08-18 13:43:08 +0200

    Update team.js

--- interactions/slash/misc/team.js
+++ interactions/slash/misc/team.js
@@ -126,7 +126,7 @@ module.exports = {
                                         { name: " ", value: " ", inline: false },
                                         { name: "Status", value: "`Access Denied ❌`", inline: true },
                                         { name: "Required Role", value: "<@&" + communityAdminRoleId + ">", inline: true },
-                                        { name: "Problem Detected", value: "Your access to the `/admin` command has been denied. You can only use this if you have the required admin role in this community. If you usually have access to this command, make sure you're in the right community or contact an admin of the bot.", inline: false },
+                                        { name: "Problem Detected", value: "Your access to the `/team` command has been denied. You can only use this if you have the required admin role in this community. If you usually have access to this command, make sure you're in the right community or contact an admin of the bot.", inline: false },
                                     )
                                     .setTimestamp()
                                     .setFooter({ text: 'Powered by Rolls Chasers', iconURL: 'https://cdn.discordapp.com/attachments/1108757872315219968/1121978623436521514/rc_logo.png' })

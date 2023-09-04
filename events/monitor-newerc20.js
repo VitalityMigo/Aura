644b29c2f9299afa0cf468d5b8beb49d185b2c38
VitalityMigo <catwalk7th@gmail.com>
2023-09-04 19:50:03 +0200

    Added new erc20 contracts and newERC721 contracts

---
+++ events/monitor-newerc20.js
@@ -0,0 +1,306 @@
+const { EmbedBuilder } = require("discord.js");
+const { apimonitorsql, apiproviderssql, adminsql, paymentHistory, accessSql, interactionData, reportsql, sequelize } = require('./database')
+
+const erc20Standard = require("../contracts/uniswap/erc20standart.json")
+const erc721Standard = require("../contracts/blur/erc721standard.json")
+
+const formatCoinValueSign = require("../functions/formatNumberEmbed")
+const reduceText = require("../functions/reducetext")
+const contractType = require("../functions/contracttype")
+
+//Récupérer les clefs API
+const dotenv = require("dotenv")
+dotenv.config()
+const infuraApiKey = process.env.infuraApiKey
+const etherscanApiKey = process.env.etherscanApiKey
+
+
+
+const Web3 = require('web3');
+const web3 = new Web3('wss://mainnet.infura.io/ws/v3/' + infuraApiKey);
+
+const axios = require('axios')
+
+
+
+
+
+// On définit le client et charge les channels
+const client = require('../bot'); // Chemin vers le fichier client.js
+
+let serverId = ""
+let channelNewERC20ContractId = ""
+let channelNewERC721ContractId = ""
+
+let channelNewERC20Contract = ""
+let channelNewERC721Contract = ""
+
+setTimeout(() => {
+
+    const botId = client.user.id;
+
+    if (botId == "1074328639165964368") {
+        // PROD
+
+        serverId = "1108754348818845729"
+        channelNewERC20ContractId = "1148045555667304528"
+        channelNewERC721ContractId = "1148045614920241192"
+
+    } else if (botId == "1119666128411709552") {
+        // DEV
+
+        serverId = "1071576735298113667"
+        channelNewERC20ContractId = "1104225853023461388"
+        channelNewERC721ContractId = "1104225853023461388"
+    }
+
+    const botGuild = client.guilds.cache.get(serverId);
+    channelNewERC20Contract = botGuild.channels.cache.get(channelNewERC20ContractId);
+    channelNewERC721Contract = botGuild.channels.cache.get(channelNewERC721ContractId);
+
+}, 3000);
+
+
+
+
+
+
+
+
+let contract = ""
+let type = ""
+let name = ""
+let symbol = ""
+
+let createdSince = ""
+let deployer = ""
+let ownership = ""
+let devBalance = ""
+let deployerTxnCount = ""
+let deploymentTxn = ""
+
+let supply = ""
+let totalSupply = ""
+let decimals = ""
+let owner = ""
+let deployerBalance = ""
+let ownerBalance = ""
+
+// ON lance l'écoute
+web3.eth.subscribe('newBlockHeaders', async (error, header) => {
+
+
+    try {
+
+    setTimeout(() => {
+
+    }, 1500);
+
+    const timeStamp = Date.now();
+    const actualTimestamp = parseFloat(timeStamp / 1000).toFixed(0)
+    createdSince = "<t:" + actualTimestamp + ":R>"
+
+    if (error) {
+        console.error('Erreur lors de l écoute des en-têtes de bloc :', error);
+        return;
+    }
+
+    const blockNumber = header.number;
+
+    await web3.eth.getBlock(blockNumber, true, async (error, block) => {
+        if (error) {
+            console.error('Erreur lors de la récupération du bloc :', error);
+            return;
+        }
+
+        // Parcourez les transactions du bloc
+        block.transactions.forEach(async transaction => {
+
+            //console.log(transaction);
+
+            if (transaction.to == null && transaction.input !== '0x' && transaction.value == 0) {
+
+                await web3.eth.getTransactionReceipt(transaction.hash)
+                    .then(async receipt => {
+
+
+                        if (receipt) {
+
+                            if (receipt.contractAddress !== null) {
+
+
+                                contract = receipt.contractAddress
+                                deployer = receipt.from
+                                deploymentTxn = receipt.transactionHash
+                                deployerTxnCount = (transaction.nonce + 1).toString()
+                                type = await contractType(contract)
+
+                                console.log(`🟢 Nouveau contrat déployé : ` + deploymentTxn);
+                                console.log(`L'adresse du contrat déployé est : ${contract} at  ${deploymentTxn} from ${deployer} and type ${type}`);
+
+
+                                if (type == "ERC20") {
+
+                                    const tokenContract = new web3.eth.Contract(erc20Standard, contract);
+                                    decimals = await tokenContract.methods.decimals().call();
+                                    owner = await tokenContract.methods.owner().call();
+                                    name = await tokenContract.methods.name().call();
+                                    symbol = await tokenContract.methods.symbol().call();
+                                    supply = await tokenContract.methods.totalSupply().call();
+                                    totalSupply = supply / 10 ** decimals
+                                  
+                                    const balanceOfDeployer = await tokenContract.methods.balanceOf(deployer).call();
+                                    deployerBalance = balanceOfDeployer / 10 ** decimals
+
+                                    if (owner.toLowerCase() == "0x0000000000000000000000000000000000000000" || owner.toLowerCase() == "0x000000000000000000000000000000000000dead") {
+
+                                        ownership = "✅ Renounced"
+                                        devBalance = formatCoinValueSign(deployerBalance) + " (" + parseFloat((deployerBalance / totalSupply) * 100).toFixed(1) + "%)"
+
+                                    } else {
+
+                                        if (owner.toLowerCase() != deployer.toLowerCase()) {
+
+                                            const balanceOfOwner = await tokenContract.methods.balanceOf(owner).call();
+                                            ownerBalance = balanceOfOwner / 10 ** decimals
+
+                                        }
+
+                                        ownership = "❌ Not renounced"
+                                        devBalance = formatCoinValueSign(deployerBalance + ownerBalance) + " (" + parseFloat(((deployerBalance + ownerBalance) / totalSupply) * 100).toFixed(1) + "%)"
+
+                                    }
+
+
+
+
+
+
+                                    const newERC20 = new EmbedBuilder().setColor("#060A8F")
+                                        .setTitle(reduceText(name, 40) + " (" + symbol.toUpperCase() + ")")
+                                        .setDescription(">>> A new ERC20 contract has been created")
+                                        .addFields(
+                                            { name: " ", value: " ", inline: false },
+                                            { name: "Contract", value: "`" + contract.toLowerCase() + "`", inline: false },
+                                            { name: "Supply", value: "`" + formatCoinValueSign(totalSupply, 2) + "`", inline: true },
+                                            { name: "Type", value: "`" + type.toUpperCase() + "`", inline: true },
+                                            { name: "Dev. Txn Count", value: "`" + deployerTxnCount + "`", inline: true },
+                                            { name: "Dev. Balance", value: "`" + devBalance + "`", inline: true },
+                                            { name: "Ownership", value: "`" + ownership + "`", inline: true },
+                                            { name: "Pair Created", value: createdSince, inline: true },
+                                            { name: "Contract", value: '[Etherscan](https://etherscan.io/address/' + contract + ") ∙ " + '[DexScreener](https://dexscreener.com/ethereum/' + contract + ") ∙ " + '[DexSpy](https://dexspy.io/eth/token/' + contract + ") ∙ " + '[Uniswap](https://app.uniswap.org/#/tokens/ethereum/' + contract + ") ∙ " + '[DefiLlama](https://swap.defillama.com/?chain=ethereum&from=0x0000000000000000000000000000000000000000&to=' + contract + ") ∙ " + '[Honeypot](   https://honeypot.is/ethereum?address=' + contract + ") ∙ " + '[Holders](https://etherscan.io/token/tokenholderchart/' + contract + ")", inline: false },
+
+
+                                        )
+                                        .setTimestamp()
+                                        .setFooter({ text: 'Powered by Rolls Chasers', iconURL: 'https://cdn.discordapp.com/attachments/1108757872315219968/1121978623436521514/rc_logo.png' });
+
+
+
+                                    await channelNewERC20Contract.send({ embeds: [newERC20] });
+
+
+
+
+
+                                } else if (type == "ERC721") {
+
+
+
+                                    const tokenContract = new web3.eth.Contract(erc721Standard, contract);
+                                    owner = await tokenContract.methods.owner().call();
+                                    name = await tokenContract.methods.name().call();
+                                    symbol = await tokenContract.methods.symbol().call();
+                                    totalSupply = await tokenContract.methods.totalSupply().call();
+                                  
+                                    const balanceOfDeployer = await tokenContract.methods.balanceOf(deployer).call();
+                                    deployerBalance = balanceOfDeployer 
+
+                                    if (owner.toLowerCase() == "0x0000000000000000000000000000000000000000" || owner.toLowerCase() == "0x000000000000000000000000000000000000dead") {
+
+                                        ownership = "✅ Renounced"
+                                        devBalance = formatCoinValueSign(deployerBalance) + " (" + parseFloat((deployerBalance / totalSupply) * 100).toFixed(1) + "%)"
+
+                                    } else {
+
+                                        if (owner.toLowerCase() != deployer.toLowerCase()) {
+
+                                            const balanceOfOwner = await tokenContract.methods.balanceOf(owner).call();
+                                            ownerBalance = balanceOfOwner
+
+                                        }
+
+                                        ownership = "❌ Not renounced"
+                                        devBalance = formatCoinValueSign(deployerBalance + ownerBalance) + " (" + parseFloat(((deployerBalance + ownerBalance) / totalSupply) * 100).toFixed(1) + "%)"
+
+                                    }
+
+
+
+
+
+
+                                    const newERC20 = new EmbedBuilder().setColor("#060A8F")
+                                        .setTitle(name)
+                                        .setDescription(">>> A new ERC721 contract has been created")
+                                        .addFields(
+                                            { name: " ", value: " ", inline: false },
+                                            { name: "Contract", value: "`" + contract.toLowerCase() + "`", inline: false },
+                                            { name: "Supply", value: "`" + formatCoinValueSign(totalSupply, 2) + "`", inline: true },
+                                            { name: "Type", value: "`" + type.toUpperCase() + "`", inline: true },
+                                            { name: "Dev. Txn Count", value: "`" + deployerTxnCount + "`", inline: true },
+                                            { name: "Dev. Balance", value: "`" + devBalance + "`", inline: true },
+                                            { name: "Ownership", value: "`" + ownership + "`", inline: true },
+                                            { name: "Contract Created", value: createdSince, inline: true },
+                                            { name: "Links", value: '[Etherscan](https://etherscan.io/address/' + contract + ") ∙ " + '[Opensea](https://opensea.io/collection/' + contract + ") ∙ " + '[Blur](https://blur.io/collection/' + contract + ") ∙ " + '[Magically](https://magically.gg/collection/' + contract + ") ∙ " + '[Holders](https://blur.io/collection/' + contract + "/holders) ∙ " + '[Deployer](https://etherscan.io/address/' + contract + ")", inline: false },
+
+
+                                        )
+                                        .setTimestamp()
+                                        .setFooter({ text: 'Powered by Rolls Chasers', iconURL: 'https://cdn.discordapp.com/attachments/1108757872315219968/1121978623436521514/rc_logo.png' });
+
+
+
+                                    await channelNewERC721Contract.send({ embeds: [newERC20] });
+
+
+
+
+                                }
+
+
+
+                            } else {
+                                console.log(`⚠️ Pas contrat : ` + transaction.hash);
+                            }
+
+                        }
+                    })
+                    .catch(error => {
+                        console.error('Erreur lors de la récupération du reçu de la transaction :', error);
+                    });
+
+
+
+                // Faites quelque chose avec l'adresse du nouveau contrat, par exemple, enregistrez-la dans une base de données.
+            } else {
+                console.log(`🔴 Pas contrat : ` + transaction.hash);
+
+                // Faites quelque chose avec l'adresse du nouveau contrat, par exemple, enregistrez-la dans une base de données.
+            }
+
+
+        });
+    });
+
+} catch (error) {
+    console.log(error);
+
+    // Faites quelque chose avec l'adresse du nouveau contrat, par exemple, enregistrez-la dans une base de données.
+}
+
+
+
+});
+
+
---
+++ functions/contracttype.js
@@ -0,0 +1,45 @@
+var Web3 = require("web3")
+const web3 = new Web3("https://cloudflare-eth.com")
+const erc721Standard = require("../contracts/blur/erc721standard.json")
+const erc20Standard = require("../contracts/uniswap/erc20standart.json")
+
+async function identifyContract(txn) {
+    let state = "???"
+    console.log("contract given to function =" + txn)
+
+    const tokenContract = new web3.eth.Contract([...erc20Standard, ...erc721Standard], txn);
+
+    try {
+        await tokenContract.methods.decimals().call();
+        state = "ERC20";
+    } catch (error) {
+        try {
+            await tokenContract.methods.ownerOf("1").call();
+            state = "ERC721";
+        } catch (error) {
+            state = "Other";
+        }
+    }
+
+    return state;
+}
+
+async function contractType(contractAddress) {
+
+    try {
+        const type = await identifyContract(contractAddress);
+        console.log("Contract Type:", type);
+
+        return type
+    } catch (error) {
+        console.error("Error:", error);
+
+        let errorMessage = 'N/A'
+return errorMessage
+    }
+}
+
+
+module.exports = contractType;
+
+

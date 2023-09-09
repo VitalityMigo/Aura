c43e37e1ecf4576a4bb412faf07d4d1a99e136fc
VitalityMigo <catwalk7th@gmail.com>
2023-09-09 21:18:23 +0200

    Update coin.js

--- interactions/slash/misc/coin.js
+++ interactions/slash/misc/coin.js
@@ -567,7 +567,7 @@ module.exports = {
                                             let part3 = value + "$ (" + share + "%)`\n"
 
                                             let spaceSize = 16 - ((part2.toString()).length)
-                                            if (sign !== "")  { spaceSize = 14 - ((part2.toString()).length) }
+                                            if (sign !== "")  { spaceSize = 13 - ((part2.toString()).length) }
                                             let spaceLenght = ""
                                             for (let i = 0; i < spaceSize; i++) { spaceLenght += " " }
 

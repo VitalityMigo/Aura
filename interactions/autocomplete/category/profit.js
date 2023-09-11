e54529c9bdacbd624e0570c1bed48ccddab8f83b
VitalityMigo <catwalk7th@gmail.com>
2023-09-11 23:52:45 +0200

    Update profit.js

--- interactions/autocomplete/category/profit.js
+++ interactions/autocomplete/category/profit.js
@@ -113,10 +113,11 @@ module.exports = {
                 const collectionTable = []
     
     
-                const popularCollectionsLink = "https://api-mainnet.magiceden.dev/v2/ord/btc/popular_collections?window=7d&limit=600"
-                const popularCollectionsCall = await axios.get(popularCollectionsLink, { headers });
-                const result = await popularCollectionsCall.data;
-    
+                // //BUG FIX 11/09/2023 - API ME BUG
+                //const popularCollectionsLink = "https://api-mainnet.magiceden.dev/v2/ord/btc/popular_collections?window=7d&limit=600"
+                //const popularCollectionsCall = await axios.get(popularCollectionsLink, { headers });
+               // const result = await popularCollectionsCall.data;
+               const result = []
 
                 if (focusedValue == "") {
 

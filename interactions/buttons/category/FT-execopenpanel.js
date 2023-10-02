6430b4939a94385b23158d6a46ed5ac124ed941b
VitalityMigo <catwalk7th@gmail.com>
2023-10-02 23:21:57 +0200

    Update FT-execopenpanel.js

--- interactions/buttons/category/FT-execopenpanel.js
+++ interactions/buttons/category/FT-execopenpanel.js
@@ -354,7 +354,7 @@ module.exports = {
                         .setTimestamp()
                         .setFooter({ text: 'Powered by Rolls Chasers', iconURL: 'https://cdn.discordapp.com/attachments/1108757872315219968/1121978623436521514/rc_logo.png' })
 
-                    await interaction.reply({ embeds: [errorNotEthereum], ephemeral: true });
+                    await interaction.editReply({ embeds: [errorNotEthereum], ephemeral: true });
 
 
                 }

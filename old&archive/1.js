edb6ef73837f605350c432c0fcc218460bdb9fd3
VitalityMigo <catwalk7th@gmail.com>
2023-11-23 13:06:42 +0100

    u

--- old&archive/1.js
+++ old&archive/1.js
@@ -6,7 +6,7 @@
 // Bouh
 
 const { EmbedBuilder, SlashCommandBuilder } = require('discord.js');
-const { accessSql, profileData, adminsql, reportsql, usersql, sequelize } = require('../../../events/database');
+const { accessSql, profileData, adminsql, reportsql, usersql, sequelize } = require('../events/database');
 const moment = require('moment');
 
 module.exports = {
@@ -115,7 +115,7 @@ module.exports = {
 
 
 
-            const reduceText = require("../../../functions/reducetext")
+            const reduceText = require("../functions/reducetext")
             const roleTag = "1121510423687090186"
 
 

5bba88822fff0b1d11b84fdd0ec9b06c7deb801a
VitalityMigo <catwalk7th@gmail.com>
2024-04-06 17:25:08 +0200

    Update fontconfig.js

--- config/fontconfig.js
+++ config/fontconfig.js
@@ -6,7 +6,7 @@ const { registerFont } = require('canvas');
 registerFont("./visual/rollschasers/font/sftransrobotic.ttf", { family: "SFTransrobotic" })
 registerFont("./visual/embassy/font/akira.ttf", { family: "EmbassyGothic" })
 registerFont("./visual/eglfamily/font/roboto.ttf", { family: "roboto" })
-registerFont("./visual/eglfamily/font/robotovr.ttf", { family: "rbt" })
+//registerFont("./visual/eglfamily/font/robotovr.ttf", { family: "rbt" })
 registerFont("./visual/alphabirds/font/utmfutura.ttf", { family: "UTM Futura Extra", weight: 'extra-bold' })
 registerFont("./visual/aura/font/opt.ttf", { family: "O PTIImprovNewWideNine,O" })
 registerFont("./visual/taproot/font/luckiest.ttf", { family: "Luckiest" })

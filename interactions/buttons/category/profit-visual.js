279e24d26658cca1bcf3feae533c70d8b822d2a4
VitalityMigo <catwalk7th@gmail.com>
2024-04-06 17:13:49 +0200

    Update profit-visual.js

--- interactions/buttons/category/profit-visual.js
+++ interactions/buttons/category/profit-visual.js
@@ -29,8 +29,8 @@ const { registerFont, createCanvas, loadImage } = require('canvas');
 
 registerFont("./visual/rollschasers/font/sftransrobotic.ttf", { family: "SFTransrobotic" })
 registerFont("./visual/embassy/font/akira.ttf", { family: "EmbassyGothic" })
-// registerFont("./visual/eglfamily/font/roboto.ttf", { family: "roboto" })
-// registerFont("./visual/eglfamily/font/robotovr.ttf", { family: "rbt" })
+registerFont("./visual/eglfamily/font/roboto.ttf", { family: "roboto" })
+registerFont("./visual/eglfamily/font/robotovr.ttf", { family: "rbt" })
 registerFont("./visual/alphabirds/font/utmfutura.ttf", { family: "UTM Futura Extra", weight: 'extra-bold' })
 registerFont("./visual/aura/font/opt.ttf", { family: "O PTIImprovNewWideNine,O" })
 
